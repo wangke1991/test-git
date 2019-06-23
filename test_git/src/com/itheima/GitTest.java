54d5fb0c341dfd2e6652a243c6178c40f596742a
wangke <wangke@qq.com>
2019-06-23 20:14:29 +0800

    git被修改了

--- test_git/src/com/itheima/GitTest.java
+++ test_git/src/com/itheima/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git被创建了");
     }
+    public void test1(){
+        System.out.println("修改");
+    }
 }
